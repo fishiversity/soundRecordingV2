ced9e3b98a2701b5dde39ec856dc1def4f06f2ab
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-01-15 14:38:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+let data: number[] = []
+// Record about 1.3 seconds of sound
+input.onButtonPressed(Button.A, function () {
+    data = []
+    for (let index = 0; index < 20000; index++) {
+        data.push(pins.analogReadPin(AnalogPin.P6))
+        basic.pause(80)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= data.length; index++) {
+        let list: number[] = []
+        pins.analogSetPeriod(AnalogPin.P0, list[index])
+    }
 })
